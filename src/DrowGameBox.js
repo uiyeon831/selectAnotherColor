20fb907dbd60426bc5a25993c916985a53055f0c
uiyeon831 <104365642+uiyeon831@users.noreply.github.com>
2024-01-16 23:25:48 +0900

    add stage, timer, score
    add stage, timer, score

--- src/DrowGameBox.js
+++ src/DrowGameBox.js
@@ -1,19 +1,63 @@
-function DrowGameBox (stage) {
-  const stageBox = Math.pow(Math.floor((stage.stage/2) + 1), 2);
-  const pcs = stage.stage === 1 ? 2 : Math.sqrt(stageBox);
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+function DrowGameBox ({stage, setStage, time, setTime, score, setScore}) {
+  const [color, setColor] = useState('#000000');
+  const [answerI, setAnswerI] = useState(1);
+  const [answerJ, setAnswerJ] = useState(1);
+
+  const stageBox = Math.pow(Math.floor((stage/2) + 1), 2);
+  const pcs = stage === 1 ? 2 : Math.sqrt(stageBox);
 
   let boxSize = [];
   for(let i = 1; i <= pcs; i++){
     boxSize.push(1);
   }
-  
+
+  function getrandomColor(){
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    if(randomColor.length === 7 && randomColor !== '#ffffff') {
+      return randomColor;
+    } else {
+      return '#adadad';
+    }
+  }
+
+  useEffect(()=>{
+    setColor(getrandomColor());
+    setAnswerI(Math.floor(Math.random()*pcs + 1));
+    setAnswerJ(Math.floor(Math.random()*pcs + 1));
+  },[stage])
+
+
+  const _click = (i, j) => {
+    if(i === answerI && j === answerJ){
+      setScore(score + (stage*time));
+      setStage(stage + 1);
+      setTime(15);
+    } else if (time > 0) {
+      setTime(time - 3);
+    }
+  }
+
   return(
     <>
       { 
-        boxSize.map(()=>{
+        boxSize.map((element, index)=>{
           return(<div className="row">
-            {boxSize.map(()=>{
-              return <div className="cell" />
+            {boxSize.map((boxElement, boxIndex)=>{
+              if(index+1 === answerI && boxIndex+1 === answerJ) {
+                return <Cell 
+                $randomColor={color}
+                className="answer"
+                onClick={()=>{_click(index+1, boxIndex+1)}} 
+              />
+              }else {
+                return <Cell 
+                  $randomColor={color}  
+                  onClick={()=>{_click(index+1, boxIndex+1)}} 
+                />
+              }
             })}
           </div>)
         })
@@ -23,3 +67,18 @@ function DrowGameBox (stage) {
 }
 
 export default DrowGameBox;
+
+
+const Cell = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 0.3em;
+  background-color: ${(props) => (props.$randomColor && props.$randomColor)};
+  &:hover{
+    cursor: pointer;
+  }
+
+  &.answer {
+    opacity: 0.5;
+  }
+`
--- src/GameBox.js
+++ src/GameBox.js
@@ -1,18 +1,46 @@
 import { styled } from "styled-components";
 import DrowGameBox from "./DrowGameBox";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function GameBox () {
-  const [stage, setStage] = useState(1)
+  const [stage, setStage] = useState(1);
+  const [time, setTime] = useState(15);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    const timer = time > 0 && setTimeout(() => setTime(time - 1), 1000);
+    return () => clearTimeout(timer);
+  }, [time]);
+
+  useEffect(() => {
+    console.log('score: ', score)
+  },[score])
 
   return(
     <>
-      <Title>
-        <p>색이 다른 네모를 찾아보세요!</p>
-      </Title>
+      <TopText>
+        <div>
+          <p>색이 다른 네모를 찾아보세요!</p>
+        </div>
+        <div className="alarm">
+          <div className="content">
+            <div className="num">{time>0 ? time : 'time over'}</div> <div className="text">{time>0 ? '초' : ''}</div>
+          </div>
+          <div className="content">
+            <div className="num">{stage}</div> <div className="text">단계</div>
+          </div>
+        </div>
+      </TopText>
       <Container>
         <div className="box">
-          <DrowGameBox stage={stage} />
+          <DrowGameBox 
+            stage={stage} 
+            setStage={setStage} 
+            time={time}
+            setTime={setTime}
+            score={score}
+            setScore={setScore}
+          />
         </div>
       </Container>
     </>
@@ -24,8 +52,29 @@ export default GameBox;
 
 
 
-const Title = styled.div`
+const TopText = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.7em;
+  width: 480px;
 
+  .alarm {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .content {
+    display: flex;
+    gap: 0.3em;
+  } 
+
+  .num {
+    font-size: 2.3em;
+  }
+
+  .text {
+    line-height: 3.1em;
+  }
 `
 
 
@@ -46,15 +95,5 @@ const Container = styled.div`
     width: 100%;
     height: 100%;
     gap: 0.2em;
-  }
-
-  .cell {
-    width: 100%;
-    height: 100%;
-    border-radius: 0.3em;
-    background-color: gray;
-    &:hover{
-      cursor: pointer;
-    }
   }
 `
